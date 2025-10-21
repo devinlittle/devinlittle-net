ffa876e8f1d87f6c06c9976f97ff3b5f580c0f90
Devin Little <devinlittlee@gmail.com>
2025-10-21 17:53:09 -0400

    feat: added will section

---
+++ src/routes/will/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+  <title>Hello there Will.</title>
+</svelte:head>
+
+<p>I hope this message finds you well</p>
+
+<p>
+  *clears throat* Ahemm, It is I, your secret admirer. Reading about you has
+  made face RED from excitement, GOD I WISH I COULD FISH ALONG SIDE YOU AND MY
+  BEAUTIFUL GIRL FRIEND. Speaking of which, let us introduce ourselves. I am
+  Devin Nicoholas Little, standing at tall 5”2 yet still, in some circumstances
+  I end up being 6”2. Devin Little’s the name, but we have a new comer on the
+  block, TRINITY!!! hi that’s me. one cool thing about me is the first letter of
+  my name is T and the last is Y! Im gonna cut you off right there titty!
+</p>
+<p>
+  So will…ive heard from a lot of my sources that you declined the offer
+  profusely about our (play) dates. Why wont you (play) date me? Anyways,
+  fishing. Sometimes i Mix up the words fishing and fisting, but that okay.
+  Would you like to fist me? Or fish me? Both great options. “For Will, gambling
+  isn’t just about the potential winnings” (Bio from you) well our relationship
+  is.
+</p>
+
+<footer>
+  Anyways shoot US an email if you ever want to continue this conversation,
+  dearest devin + titty. 2025/10/21
+</footer>
+
+<p><a href="mailto:devin.little@hawks.tech">My Professional Email</a></p>
+<p><a href="mailto:trinity.mcmahon@hawks.tech">Her Professional Email</a></p>
